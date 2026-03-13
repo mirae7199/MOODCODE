98fbaf00e62b9d2e86199bb0c835a13a0c8ead91
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-13 12:59:20 +0900

    [MOODCODE] feat: 기능 추가
    - cartId로 cartItem 목록 조회
    - cartItemIds와 userId로 cartItem 목록 조회
    - productOptionIds로 해당하는 상품 삭제

--- src/main/java/com/devcrew/moodcode/domain/cart/repository/CartItemRepository.java
+++ src/main/java/com/devcrew/moodcode/domain/cart/repository/CartItemRepository.java
@@ -1,13 +1,17 @@
 package com.devcrew.moodcode.domain.cart.repository;
 
 import com.devcrew.moodcode.domain.cart.CartItem;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
   Optional<CartItem> findByProductOptionId(Long productOptionId);
-
+  List<CartItem> findByCart_Id(Long cartId);
   boolean existsByCartIdAndProductOptionId(Long cartId, Long productOptionId);
-
+  List<CartItem> findByIdInAndCart_User_Id(List<Long> cartItemIds, Long userId);
+  void deleteAllByProductOptionIdIn(List<Long> productOptionIds);
 }
