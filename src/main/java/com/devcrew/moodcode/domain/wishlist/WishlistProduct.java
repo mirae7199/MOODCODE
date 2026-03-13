80974ac0d6f5c708f88d340627d68d0676e6c585
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-13 13:06:08 +0900

    [MOODCODE] feat: product 패키지 변경

--- src/main/java/com/devcrew/moodcode/domain/wishlist/WishlistProduct.java
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/WishlistProduct.java
@@ -1,7 +1,7 @@
 package com.devcrew.moodcode.domain.wishlist;
 
+import com.devcrew.moodcode.domain.product.Product;
 import com.devcrew.moodcode.global.common.BaseTimeEntity;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,11 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
