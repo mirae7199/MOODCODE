ea78edcea6319b124ddc437d1417101abca5c520
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-13 12:39:19 +0900

    [MOODCODE] feat: ordersController 구현
    - 장바구니에서 주문 생성 API 구현
    - 사용자의 주문 목록 조회 API 구현
    - 사용자의 주문 목록 상세 조회 API 구현

---
+++ src/main/java/com/devcrew/moodcode/domain/order/controller/OrdersController.java
@@ -0,0 +1,59 @@
+package com.devcrew.moodcode.domain.order.controller;
+
+import com.devcrew.moodcode.domain.order.dto.FindOrderDetailResponse;
+import com.devcrew.moodcode.domain.order.dto.FindOrdersResponse;
+import com.devcrew.moodcode.domain.order.dto.RegisterCartItemOrderRequest;
+import com.devcrew.moodcode.domain.order.service.OrderService;
+import com.devcrew.moodcode.global.auth.LoginUser;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/orders")
+public class OrdersController {
+
+  private final OrderService orderService;
+
+  @PostMapping
+  public ResponseEntity<String> cartItemOrder(
+      @LoginUser Long userId,
+      @RequestBody @Valid RegisterCartItemOrderRequest request
+  ) {
+    String orderNo = orderService.createCartItemOrder(
+        userId,
+        request.toCommand()
+    );
+
+    return ResponseEntity.ok(orderNo);
+
+  }
+
+  @GetMapping
+  public ResponseEntity<FindOrdersResponse> getOrders(
+      @LoginUser Long userId
+  ) {
+    FindOrdersResponse response = orderService.getOrders(userId);
+
+    return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/{orderNo}")
+  public ResponseEntity<FindOrderDetailResponse> getOrdersDetail(
+      @LoginUser Long userId,
+      @PathVariable String orderNo
+  ) {
+    FindOrderDetailResponse response = orderService.getOrdersDetail(userId, orderNo);
+
+    return ResponseEntity.ok(response);
+
+  }
+}
+
